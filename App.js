8fae6378f396ec32aac9602584b143d6c598fb8c
XTS <johndoe@example.com>
2023-01-13 12:23:07 +0300

    intial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import * as math from "mathjs";
 
 import "./App.css";
 import Button from "./components/Button";
-import Input from "./components/Input";
+import Input from "./components/";
 
 const App = () => {
   const [text, setText] = useState("");
